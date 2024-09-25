a9d06300c0b6d18e9653729fc511fcf1f483ac97
ArChrisVa <arvachri@csd.auth.gr>
2024-09-25 19:45:25 +0300

    Created a Handling editor component

--- web/src/components/Handling_editor.tsx
+++ web/src/components/Handling_editor.tsx
@@ -1,19 +1,26 @@
-import React from 'react'
-import { Box } from '@chakra-ui/react'
+import { Box, Container, Divider, Text, VStack } from '@chakra-ui/react'
+import Navbar_left_editor from './Navbar_left_editor'
+import Attribute_editor from './Attributes_editor'
 
 function Handling_editor() {
   return (
-    <div>
-      <Box
-        w="100%"
+    <Container 
         h="100%"
-        bg="#474747"
-        border="1px"
-        borderStyle="round"
-      >
-        Hi
-      </Box>
-    </div>
+        p={2}
+        backgroundColor= "rgba(26, 32, 44, 0.9)"
+        >
+        <Box>
+            <VStack spacing="8">
+                <Text fontSize = "2xl" align="center" pt="4" color="white">
+                    Handling Editor
+                </Text>
+                <Divider />
+                <Navbar_left_editor />
+                <Divider />
+                <Attribute_editor />
+            </VStack>
+        </Box>
+    </Container>
   )
 }
 
