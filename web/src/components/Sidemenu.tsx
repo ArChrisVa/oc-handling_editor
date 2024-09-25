c9c3a2e9658c306ab85bb01a13222087f517311e
ArChrisVa <arvachri@csd.auth.gr>
2024-09-25 19:44:11 +0300

    Created a Sidemenu component

---
+++ web/src/components/Sidemenu.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Box, Container } from '@chakra-ui/react'
+
+function Sidemenu() {
+  return (
+    <Container
+        h="100%"
+        p={2}
+        backgroundColor= "rgba(26, 32, 44, 0.4)"
+    >
+        <Box>
+            <h1>Sidemenu</h1>
+        </Box>
+    </Container>
+  )
+}
+
+export default Sidemenu
